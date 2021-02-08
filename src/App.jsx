1928ef8abf632fd337e4e19a5f41c06f16ac4cb3
Dan6erbond <moravrav@gmail.com>
2021-02-08 10:42:39 +0100

    Implement nav with mobile dropdown implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import code from "./assets/img/code.png";
+import "./App.css";
+import * as React from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import clsx from "clsx";
 
 function App() {
+  const [navOpen, setNavOpen] = React.useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="app">
+        {/* This is where you would usually wrap your content in a general <Layout/> component. */}
+        <main>
+          <Switch>
+            <Route path="/">Home</Route>
+            <Route path="/projects">Projects</Route>
+            <Route path="/about">About</Route>
+            <Route path="/contact">Contact</Route>
+          </Switch>
+        </main>
+        <nav className={clsx(navOpen && "open")}>
+          <img src={code} alt="Code" onClick={() => setNavOpen(!navOpen)} />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Me</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
